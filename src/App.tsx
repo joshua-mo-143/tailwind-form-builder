09c1c2a9e11ef7fd1f7b95b5b2dd3e9c34a08b5b
Josh Mo <joshua.om.876@gmail.com>
2022-10-28 20:54:52 +0100

    e

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AllPages from './pages/AllPages';
+import FormBuilder from './pages/FormBuilder';
 
 export const App: React.FC = () => {
 
   return (
-    <BrowserRouter>
-    <AllPages/>
-    </BrowserRouter>
+    <FormBuilder/>
   )
 }
 
